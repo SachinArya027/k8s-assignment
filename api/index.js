d2327edfff31adb909dfe766fff9266850d06f1b
Sachin Arya <sachin.arya027@gmail.com>
2024-05-25 00:12:30 +0530

    initial commit

---
+++ api/index.js
@@ -0,0 +1,31 @@
+// Node.js
+import express from 'express';
+import { Pool } from 'pg';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+const pool = new Pool({
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+});
+
+app.get('/test', (req, res) => {
+  res.send('hi there !!!');
+});
+
+app.get('/', (req, res) => {
+  pool.query('SELECT NOW()', (err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(result.rows);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
